e82cb82ba89c7f63f925eb44b4c2212a3b40f992
Yamil Orozco <yamiloro@gmail.com>
2020-03-03 22:17:01 -0430

    adding new feachure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import Routes from './routes';
-
+import Context from './utils/context';
+import * as Reducer from './store/hooks_state/hooks_reducer';
+import * as ACTION from './store/actions/actions';
 
 //main app 
 const App = () => {
 
-  
-    return(
-      <div>
+  const [stateGlobal, setStateGlobal] = useState(0);
+  const [stateContextGlobal, dispatchContextGlobal] = useReducer(Reducer.HooksReducer, Reducer.initialState);
+
+  const incrementGlobalState = () => {
+    setStateGlobal(stateGlobal + 1);
+  }
+
+  const decrementGlobalState = () => {
+    setStateGlobal(stateGlobal - 1);
+  }
+
+
+  const handleContextDispatchTrue = () => {
+    dispatchContextGlobal(ACTION.success());
+  }
+  const handleContextDispatchFalse = () => {
+    dispatchContextGlobal(ACTION.failure());
+  }
+  return (
+    <div>
       React
-      <Routes />
-      </div>
-    )
+      <Context.Provider value={{
+        valueGlobalState: stateGlobal,
+        addGlobalValue: () => incrementGlobalState(),
+        decGlobalValue: () => decrementGlobalState(),
+        reducerGlobalState: stateContextGlobal.stateprop2,
+        dispatchContextTrue: () =>handleContextDispatchTrue(),
+        dispatchContextFalse: () =>handleContextDispatchFalse(),
+
+      }} >
+        <Routes />
+      </Context.Provider>
+    </div>
+  )
 }
 
 
--- src/hooks/hooks_container1.js
+++ src/hooks/hooks_container1.js
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useReducer, useContext } from 'react';
+import * as Reducer from '../store/hooks_state/hooks_reducer';
+import * as ACTION from '../store/actions/actions';
+import Context from '../utils/context';
 
 
 //main app 
 const HooksContainer1 = () => {
 
+    const context = useContext(Context);
 
     const [stateValue, setValue] = useState(0);
     const incrementValue = () => {
@@ -14,7 +17,10 @@ const HooksContainer1 = () => {
         setValue(stateValue - 1);
     }
 
+    const [state, dispatch] = useReducer(Reducer.HooksReducer, Reducer.initialState);
+
     const [useEffectValue, setUseEffectValue] = useState(null);
+
 
     useEffect(() => {
         setTimeout(() => setUseEffectValue('useEfect Worked'), 3000);
@@ -23,6 +29,13 @@ const HooksContainer1 = () => {
     const changeuseEffectvalue = () => {
         setUseEffectValue('some string');
     }
+
+    const handleDispatchTrue = () => {
+        dispatch(ACTION.success());
+    }
+    const handleDispatchFalse = () => {
+        dispatch(ACTION.failure());
+    }
     return (
         <div>
             React Hooks
@@ -30,6 +43,12 @@ const HooksContainer1 = () => {
             <button onClick={() => incrementValue()} > Inc Local State</button>
             <button onClick={() => decrementValue()} > Inc Local State</button>
             <button onClick={() =>changeuseEffectvalue()} > Change use effect</button>
+            <button onClick={() =>handleDispatchTrue()} > dispatch true</button>
+            <button onClick={() =>handleDispatchFalse()} > dispatch false </button>
+            <button onClick={() =>context.addGlobalValue()} >add global </button>
+            <button onClick={() =>context.decGlobalValue()} >dec global </button>
+            <button onClick={() =>context.dispatchContextTrue()} >True Global Context </button>
+            <button onClick={() =>context.dispatchContextFalse()} >False Global Context </button>
             <br />
             <div>
                 <br />
@@ -38,7 +57,22 @@ const HooksContainer1 = () => {
                     <p>No value</p>
                 }
                 <br />
+                {
+                    state.stateprop1
+                    ? <p> state prop1 is true</p>
+                    : <p> state prop1 is false</p>
+                }
+                <br />
+                {
+                   context.reducerGlobalState
+                    ? <p> reducer state prop2 is true</p>
+                    : <p> reducer state prop2 is false</p>
+                }
+                <br />
+
                 <p>Local State: {stateValue}</p>
+                <br />
+                <p>Global State: {context.valueGlobalState}</p>
             </div>
         </div>
     )
